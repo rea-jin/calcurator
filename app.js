41cc5eec1fa2c938c1bf9312bfe6fc25e9c06203
rea-jin <56153015+rea-jin@users.noreply.github.com>
2021-10-06 12:51:28 +0900

    second commit

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ $(function () {
     var num = $(this).val();
     // 数字の時
     if (!isNaN(num)) {
-    	// 演算子が空の時
-    	if(kigou === ""){
-    		// 計算結果を空にする
-    		answer = "";
-    		}
-    		
+      // 演算子が空の時
+      if (kigou === "") {
+        // 計算結果を空にする
+        answer = "";
+      }
+
       console.log(num);
       // 数字なら、totalに結合
       total = total + num;
@@ -49,38 +49,38 @@ $(function () {
           break;
 
         case "minus":
-         console.log(num);
-         kigou = "-";
-         keisan(answer);
-         break;
+          console.log(num);
+          kigou = "-";
+          keisan(answer);
+          break;
 
         case "times":
-         console.log(num);
-         kigou = "*";
-         keisan(answer);
-         break;
+          console.log(num);
+          kigou = "*";
+          keisan(answer);
+          break;
 
         case "divide":
-         console.log(num);
-         kigou = "/";
-         keisan(answer);
-         break;
+          console.log(num);
+          kigou = "/";
+          keisan(answer);
+          break;
 
         case "calc":
           // 結果は,入力保持された値 記号 入力中の値
           console.log("a" + answer + "v" + Val + "t" + total);
           Val = parseFloat(Val);
           total = parseFloat(total);
-          
+
           // 答えが空の時
-          if(answer === ""){
-         	 answer =  Val + kigou + total;
- 		  
- 		  // 答えが空でないとき
- 		  }else{
- 		  	answer = answer + kigou + total;
- 		  }
-          
+          if (answer === "") {
+            answer = Val + kigou + total;
+
+            // 答えが空でないとき
+          } else {
+            answer = answer + kigou + total;
+          }
+
           // 数値変換
           answer = eval(answer);
           answer = parseFloat(answer);
@@ -89,21 +89,20 @@ $(function () {
           kigou = "";
           break;
 
-          case "clear":
-             total = 0;
-             Val = 0;
-             answer = 0;
-             num = 0;
-             kigou = "";
-            $("input").val(num); // 初期値0を表示
-         break;
+        case "clear":
+          total = 0;
+          Val = 0;
+          answer = 0;
+          num = 0;
+          kigou = "";
+          $("input").val(num); // 初期値0を表示
+          break;
       }
-
     }
   });
 
   function keisan() {
-   // もし、answerが0でなければ、（すでに1度記号が押されている場合）計算する
+    // もし、answerが0でなければ、（すでに1度記号が押されている場合）計算する
     if (answer !== "") {
       // 値を代入
       Val = total;
@@ -115,8 +114,8 @@ $(function () {
       num = 0;
       total = 0;
     } else {
-       // 値を代入
-       Val = total;
+      // 値を代入
+      Val = total;
       // answerが0の場合
       console.log("answer:" + answer);
       num = 0;
@@ -124,5 +123,4 @@ $(function () {
       $("input").val(num);
     }
   }
-
 });
